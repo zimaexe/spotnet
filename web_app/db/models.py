352f03e5ec2f36cb552b4a276a6a8212f19b19d5
djeck1432 <oleh.kostashchuk@gmail.com>
2024-10-11 13:58:06 +0200

    add new model for creating position

--- web_app/db/models.py
+++ web_app/db/models.py
@@ -1,6 +1,7 @@
 from uuid import uuid4
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy import Column, String, Boolean
+from sqlalchemy.sql import func
+from sqlalchemy import Column, String, Boolean, Integer, ForeignKey, DateTime
 from web_app.db.database import Base
 
 
@@ -8,10 +9,27 @@ class User(Base):
     """
     SQLAlchemy model for the user table.
     """
-    __tablename__ = 'user'
+
+    __tablename__ = "user"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
     is_contract_deployed = Column(Boolean, default=False)
     wallet_id = Column(String, nullable=False, index=True)
     deployed_transaction_hash = Column(String)
 
+
+class Position(Base):
+    """
+    SQLAlchemy model for the position table.
+    """
+
+    __tablename__ = "position"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    user_id = Column(
+        UUID(as_uuid=True), ForeignKey("user.id"), index=True, nullable=False
+    )
+    token_symbol = Column(String, nullable=False)
+    amount = Column(String, nullable=False)
+    multiplier = Column(Integer, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=func.now())
