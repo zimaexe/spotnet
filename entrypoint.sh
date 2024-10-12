b1628cecdc5558cf3af6f2aea630c1232b2f55c4
djeck1432 <oleh.kostashchuk@gmail.com>
2024-10-12 20:23:28 +0200

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Run migration with alembic"
+alembic -c web_app/alembic.ini upgrade head
+
 echo "Starting the server ..."
 
 uvicorn web_app.api.main:app --host 0.0.0.0 --port 8000
