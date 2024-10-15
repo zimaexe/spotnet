fa0b730513ed693184b45ab8cb69264773ef5551
djeck1432 <oleh.kostashchuk@gmail.com>
2024-10-15 11:50:44 +0200

    add dynamic dashboard

--- web_app/api/dashboard.py
+++ web_app/api/dashboard.py
@@ -1,30 +1,37 @@
 from datetime import datetime
+import collections
 
 from fastapi import APIRouter
 from starlette.requests import Request
 
+from web_app.db.crud import PositionDBConnector
 from web_app.contract_tools.mixins.dashboard import DashboardMixin
 
 router = APIRouter()
+position_db_connector = PositionDBConnector()
 
 
 @router.get("/api/dashboard")
-async def get_dashboard(request: Request):
+async def get_dashboard(request: Request, wallet_id: str) -> dict:
     """
     Get the dashboard with the balances, multipliers, start dates, and zkLend position.
+    :param wallet_id: Wallet ID
     :param request: HTTP request
     :return: template response
     """
-    # Pass balances, multipliers, and start_dates to the template
-    wallet_id = "0x020281104e6cb5884dabcdf3be376cf4ff7b680741a7bb20e5e07c26cd4870af" # mock wallet ID for demonstration purposes
+    contract_address = position_db_connector.get_contract_address_by_wallet_id(wallet_id)
+    opened_positions = position_db_connector.get_positions_by_wallet_id(wallet_id)
+
+    first_opened_position = opened_positions[0] if opened_positions else collections.defaultdict(lambda: None)
     # Fetch zkLend position for the wallet ID
-    zklend_position = await DashboardMixin.get_zklend_position(wallet_id)
+    zklend_position = await DashboardMixin.get_zklend_position(contract_address)
 
     # Fetch balances (assuming you have a method for this)
     wallet_balances = await DashboardMixin.get_wallet_balances(wallet_id)
     return {
         "balances": wallet_balances,
-        "multipliers": {"ETH": 5},
-        "start_dates": {"ETH": datetime.now()},
+        "multipliers": {"ETH": first_opened_position["multiplier"]},
+        "start_dates": {"ETH": first_opened_position["created_at"]},
         "zklend_position": zklend_position,
+        "position_id": first_opened_position["id"],
     }
