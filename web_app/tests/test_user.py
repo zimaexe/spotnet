f71397898c1fca1aba15bb79983dc1d696bba733
vitaliypopel <vitaliypopel14@gmail.com>
2024-10-18 18:36:44 +0300

    Created working tests

--- web_app/tests/test_user.py
+++ web_app/tests/test_user.py
@@ -1,6 +1,8 @@
 import httpx
 import pytest
 
+from web_app.api.serializers.transaction import UpdateUserContractRequest
+
 
 @pytest.fixture(scope="function")
 async def async_client():
@@ -10,23 +12,61 @@ async def async_client():
 
 @pytest.mark.asyncio
 async def test_get_user_contract(async_client):
-    response = await async_client.get("/api/get-user-contract")
-    assert response.status_code == 422
+    response = await async_client.get(
+        url="/api/get-user-contract",
+        params={
+            "wallet_id": "",
+        },
+    )
+    response_json = response.json()
+
+    assert response.status_code == 200
+    assert isinstance(response_json, int)
 
 
 @pytest.mark.asyncio
 async def test_check_user(async_client):
-    response = await async_client.get("/api/check-user")
-    assert response.status_code == 422
+    response = await async_client.get(
+        url="/api/check-user",
+        params={
+            "wallet_id": "",
+        },
+    )
+    response_json = response.json()
+
+    assert response.status_code == 200
+    assert isinstance(response_json, dict)
+    assert "is_contract_deployed" in response_json
 
 
 @pytest.mark.asyncio
 async def test_change_user_contract(async_client):
-    response = await async_client.post("/api/update-user-contract")
-    assert response.status_code == 422
+    data = UpdateUserContractRequest(
+        wallet_id="",
+        contract_address="",
+    )
+
+    response = await async_client.post(
+        url="/api/update-user-contract",
+        json=data.dict(),
+    )
+    response_json = response.json()
+
+    assert response.status_code == 200
+    assert isinstance(response_json, dict)
+    assert "is_contract_deployed" in response_json
 
 
 @pytest.mark.asyncio
 async def test_get_user_contract_address(async_client):
-    response = await async_client.get("/api/get-user-contract-address")
-    assert response.status_code == 422
+    response = await async_client.get(
+        url="/api/get-user-contract-address",
+        params={
+            "wallet_id": "",
+        },
+    )
+    response_json = response.json()
+
+    assert response.status_code == 200
+    assert isinstance(response_json, dict)
+    assert "contract_address" in response_json
