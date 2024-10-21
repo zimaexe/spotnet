01e2bc1852af605f47bd3a8d217b383ba82c94de
vitaliypopel <vitaliypopel14@gmail.com>
2024-10-21 12:04:00 +0300

    Fixed GetUserContractAddressResponse annotation from str to str|None

--- web_app/api/serializers/user.py
+++ web_app/api/serializers/user.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, Field
 
+
 class CheckUserResponse(BaseModel):
     is_contract_deployed: bool = Field(..., example=False, description="Indicates if the user's contract is deployed.")
 
@@ -7,5 +8,6 @@ class CheckUserResponse(BaseModel):
 class UpdateUserContractResponse(BaseModel):
     is_contract_deployed: bool = Field(..., example=False, description="Indicates if the user's contract is deployed.")
 
+
 class GetUserContractAddressResponse(BaseModel):
-    contract_address: str = Field(None, example="0xabc123...", description="The contract address of the user, or None if not deployed.")
+    contract_address: str | None = Field(None, example="0xabc123...", description="The contract address of the user, or None if not deployed.")
