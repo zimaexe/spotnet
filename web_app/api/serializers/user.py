697486a109fafac0be20abf5bf3069f89e421da0
vitaliypopel <vitaliypopel14@gmail.com>
2024-10-21 21:23:14 +0300

    Formatted with black

--- web_app/api/serializers/user.py
+++ web_app/api/serializers/user.py
@@ -2,12 +2,20 @@ from pydantic import BaseModel, Field
 
 
 class CheckUserResponse(BaseModel):
-    is_contract_deployed: bool = Field(..., example=False, description="Indicates if the user's contract is deployed.")
+    is_contract_deployed: bool = Field(
+        ..., example=False, description="Indicates if the user's contract is deployed."
+    )
 
 
 class UpdateUserContractResponse(BaseModel):
-    is_contract_deployed: bool = Field(..., example=False, description="Indicates if the user's contract is deployed.")
+    is_contract_deployed: bool = Field(
+        ..., example=False, description="Indicates if the user's contract is deployed."
+    )
 
 
 class GetUserContractAddressResponse(BaseModel):
-    contract_address: str | None = Field(None, example="0xabc123...", description="The contract address of the user, or None if not deployed.")
+    contract_address: str | None = Field(
+        None,
+        example="0xabc123...",
+        description="The contract address of the user, or None if not deployed.",
+    )
