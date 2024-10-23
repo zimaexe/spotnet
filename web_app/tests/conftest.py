4005333a886a0889c54c5e08647c908402f672fc
vitaliypopel <vitaliypopel14@gmail.com>
2024-10-23 11:50:16 +0300

    Fixed mocking db in conftext.py

--- web_app/tests/conftest.py
+++ web_app/tests/conftest.py
@@ -1,20 +1,21 @@
 import pytest
-
 from fastapi.testclient import TestClient
 from unittest.mock import MagicMock
 
 from web_app.api.main import app
 from web_app.db.crud import DBConnector, UserDBConnector, PositionDBConnector
-from web_app.db.database import get_database, Base, engine
+from web_app.db.database import get_database
 
 
 @pytest.fixture(scope="module")
-def client(mock_db_connector, mock_user_db_connector, mock_position_db_connector):
-    Base.metadata.create_all(bind=engine)
+def client():
+    """
+    TestClient with setted mock db connection
+    :return: None
+    """
 
+    mock_db_connector = MagicMock(spec=DBConnector)
     app.dependency_overrides[get_database] = lambda: mock_db_connector
-    app.dependency_overrides[UserDBConnector] = lambda: mock_user_db_connector
-    app.dependency_overrides[PositionDBConnector] = lambda:mock_position_db_connector
 
     with TestClient(app=app) as client:
         yield client
@@ -24,17 +25,29 @@ def client(mock_db_connector, mock_user_db_connector, mock_position_db_connector
 
 @pytest.fixture(scope="module")
 def mock_db_connector():
+    """
+    Mock DBConnector
+    :return: None
+    """
     mock_connector = MagicMock(spec=DBConnector)
     yield mock_connector
 
 
 @pytest.fixture(scope="module")
 def mock_user_db_connector():
+    """
+    Mock for UserDBConnector
+    :return: None
+    """
     mock_user_connector = MagicMock(spec=UserDBConnector)
     yield mock_user_connector
 
 
 @pytest.fixture(scope="module")
 def mock_position_db_connector():
+    """
+    Mock for PositionDBConnector
+    :return: None
+    """
     mock_position_connector = MagicMock(spec=PositionDBConnector)
     yield mock_position_connector
