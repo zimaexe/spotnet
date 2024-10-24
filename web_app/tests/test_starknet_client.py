b60cae887c1acc8767568ed8591f223c11a0a125
vitaliypopel <vitaliypopel14@gmail.com>
2024-10-24 17:14:47 +0300

    Implemented all tests for StarknetClient and improved previous code

--- web_app/tests/test_starknet_client.py
+++ web_app/tests/test_starknet_client.py
@@ -1,12 +1,12 @@
-from tracemalloc import Trace
-
 import pytest
 
+from math import floor
 from starknet_py.contract import Contract
 from starknet_py.net.full_node_client import FullNodeClient
-from unittest.mock import AsyncMock, MagicMock, patch
+from unittest.mock import AsyncMock, patch
 
 from web_app.contract_tools.blockchain_call import StarknetClient
+from web_app.contract_tools.constants import TokenParams
 
 CLIENT = StarknetClient()
 
@@ -76,14 +76,14 @@ class TestStarknetClient:
     @patch.object(FullNodeClient, "call_contract", new_callable=AsyncMock)
     async def test__func_call(
         self,
-        mock_call_contract: AsyncMock,
+        mock_client_call_contract: AsyncMock,
         addr: int,
         selector: str,
         calldata: list[int],
     ) -> None:
         """
         Test cases for StarknetClient._func_call method
-        :param mock_call_contract: unittest.mock.AsyncMock
+        :param mock_client_call_contract: unittest.mock.AsyncMock
         :param addr: int
         :param selector: str
         :param calldata: list[int]
@@ -94,11 +94,11 @@ class TestStarknetClient:
             "selector": selector,
             "calldata": calldata,
         }
-        mock_call_contract.return_value = expected_response
+        mock_client_call_contract.return_value = expected_response
 
         response = await CLIENT._func_call(addr, selector, calldata)
 
-        mock_call_contract.assert_called_once()
+        mock_client_call_contract.assert_called_once()
         assert response == expected_response
 
     @pytest.mark.asyncio
@@ -135,14 +135,14 @@ class TestStarknetClient:
     @patch.object(Contract, "from_address", new_callable=AsyncMock)
     async def test__get_pool_price(
         self,
-        mock_from_contract: AsyncMock,
+        mock_contract_from_address: AsyncMock,
         token0: int,
         token1: int,
         is_token1: bool,
     ) -> None:
         """
         Test cases for StarknetClient._get_pool_price method
-        :param mock_from_contract: unittests.mock.AsyncMock
+        :param mock_contract_from_address: unittests.mock.AsyncMock
         :param token0: int
         :param token1: int
         :param is_token1: bool
@@ -156,22 +156,22 @@ class TestStarknetClient:
             "extension": 0,
         }
 
+        sqrt_ratio = sum([ord(char) for char in str(token0) + str(token1)])
+
         mock_contract = AsyncMock()
         mock_contract.functions["get_pool_price"].call = AsyncMock(
             return_value=[
                 {
-                    "sqrt_ratio": sum(
-                        [ord(char) for char in str(token0) + str(token1)]
-                    ),
+                    "sqrt_ratio": sqrt_ratio,
                 }
             ],
         )
 
-        mock_from_contract.return_value = mock_contract
+        mock_contract_from_address.return_value = mock_contract
 
         pool_price = await CLIENT._get_pool_price(pool_key, is_token1)
 
-        mock_from_contract.assert_called_once()
+        mock_contract_from_address.assert_called_once()
         mock_contract.functions["get_pool_price"].call.assert_called_once()
 
         assert pool_price
@@ -182,28 +182,28 @@ class TestStarknetClient:
         "token_addr, holder_addr, decimals",
         [
             (
-                "0x04718F5A0FC34CC1AF16A1CDEE98FFB20C31F5CD61D6AB07201858F4287C938D",
-                "0x04718F5A0FC34CC1AF16A1CDEE98FFB20C31F5CD61D6AB07201858F4287C938D",
+                TokenParams.get_token_address("STRK"),
+                TokenParams.get_token_address("STRK"),
                 18,
             ),
             (
-                "0x049D36570D4E46F48E99674BD3FCC84644DDD6B96F7C741B1562B82F9E004DC7",
-                "0x049D36570D4E46F48E99674BD3FCC84644DDD6B96F7C741B1562B82F9E004DC7",
+                TokenParams.get_token_address("ETH"),
+                TokenParams.get_token_address("ETH"),
                 18,
             ),
             (
-                "0x053C91253BC9682C04929CA02ED00B3E423F6710D2EE7E0D5EBB06F3ECF368A8",
-                "0x053C91253BC9682C04929CA02ED00B3E423F6710D2EE7E0D5EBB06F3ECF368A8",
+                TokenParams.get_token_address("USDC"),
+                TokenParams.get_token_address("USDC"),
                 6,
             ),
             (
-                "0x04718F5A0FC34CC1AF16A1CDEE98FFB20C31F5CD61D6AB07201858F4287C938D",
-                "0x049D36570D4E46F48E99674BD3FCC84644DDD6B96F7C741B1562B82F9E004DC7",
+                TokenParams.get_token_address("STRK"),
+                TokenParams.get_token_address("ETH"),
                 18,
             ),
             (
-                "0x049D36570D4E46F48E99674BD3FCC84644DDD6B96F7C741B1562B82F9E004DC7",
-                "0x053C91253BC9682C04929CA02ED00B3E423F6710D2EE7E0D5EBB06F3ECF368A8",
+                TokenParams.get_token_address("ETH"),
+                TokenParams.get_token_address("USDC"),
                 None,
             ),
         ],
@@ -211,21 +211,21 @@ class TestStarknetClient:
     @patch.object(FullNodeClient, "call_contract", new_callable=AsyncMock)
     async def test_get_balance(
         self,
-        mock_call_contract: AsyncMock,
+        mock_client_call_contract: AsyncMock,
         token_addr: str,
         holder_addr: str,
         decimals: int | None,
     ) -> None:
         """
         Test cases for StarknetClient.get_balance method
-        :param mock_call_contract: unittest.mock.AsyncMock
+        :param mock_client_call_contract: unittest.mock.AsyncMock
         :param token_addr: str
         :param holder_addr: str
         :param decimals: int | None
         :return: None
         """
         contract_return_value = [int(str(token_addr)[:10], base=16)]
-        mock_call_contract.return_value = contract_return_value
+        mock_client_call_contract.return_value = contract_return_value
 
         balance = await CLIENT.get_balance(token_addr, holder_addr, decimals)
 
@@ -236,3 +236,134 @@ class TestStarknetClient:
             assert balance == str(round(contract_return_value[0] / 10**decimals, 6))
         else:
             assert balance == str(round(contract_return_value[0], 6))
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize(
+        "deposit_token_addr, amount, multiplier, wallet_id, borrowing_token_addr",
+        [
+            (
+                TokenParams.get_token_address("STRK"),
+                "100_000",
+                2,
+                "0x27994c503bd8c32525fbdaf9d398bdd4e86757988c64581b055a06c5955ea49",
+                TokenParams.get_token_address("STRK"),
+            ),
+            (
+                TokenParams.get_token_address("USDC"),
+                "3333.3",
+                4,
+                "0x27994c503bd8c32525fbdaf9d398bdd4e86757988c64581b055a06c5955ea49",
+                TokenParams.get_token_address("ETH"),
+            ),
+        ],
+    )
+    @patch.object(Contract, "from_address", new_callable=AsyncMock)
+    async def test_get_loop_liquidity_data(
+        self,
+        mock_contract_from_address: AsyncMock,
+        deposit_token_addr: str,
+        amount: int,
+        multiplier: int,
+        wallet_id: str,
+        borrowing_token_addr: str,
+    ) -> None:
+        """
+        Test cases for StarknetClient.get_loop_liquidity_data method
+        :param mock_contract_from_address: unittest.mock.AsyncMock
+        :param deposit_token_addr: str
+        :param amount: int
+        :param multiplier: int
+        :param wallet_id: str
+        :param borrowing_token_addr:str
+        :return: None
+        """
+        sqrt_ratio = sum(
+            [ord(char) for char in deposit_token_addr + borrowing_token_addr]
+        )
+
+        mock_contract = AsyncMock()
+        mock_contract.functions["get_pool_price"].call = AsyncMock(
+            return_value=[
+                {
+                    "sqrt_ratio": sqrt_ratio,
+                }
+            ],
+        )
+        mock_contract_from_address.return_value = mock_contract
+
+        liquidity_data = await CLIENT.get_loop_liquidity_data(
+            deposit_token_addr, amount, multiplier, wallet_id, borrowing_token_addr
+        )
+
+        mock_contract_from_address.assert_called_once()
+        mock_contract.functions["get_pool_price"].call.assert_called_once()
+
+        assert liquidity_data
+        assert isinstance(liquidity_data, dict)
+        assert isinstance(liquidity_data["pool_price"], int)
+        assert liquidity_data["caller"] == CLIENT._convert_address(wallet_id)
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize(
+        "deposit_token_addr, borrowing_token_addr",
+        [
+            (
+                TokenParams.get_token_address("STRK"),
+                TokenParams.get_token_address("STRK"),
+            ),
+            (
+                TokenParams.get_token_address("ETH"),
+                TokenParams.get_token_address("ETH"),
+            ),
+            (
+                TokenParams.get_token_address("USDC"),
+                TokenParams.get_token_address("USDC"),
+            ),
+            (
+                TokenParams.get_token_address("STRK"),
+                TokenParams.get_token_address("ETH"),
+            ),
+            (
+                TokenParams.get_token_address("ETH"),
+                TokenParams.get_token_address("USDC"),
+            ),
+        ],
+    )
+    @patch.object(Contract, "from_address", new_callable=AsyncMock)
+    async def test_get_repay_data(
+        self,
+        mock_contract_from_address: AsyncMock,
+        deposit_token_addr: str,
+        borrowing_token_addr: str,
+    ) -> None:
+        """
+        Test cases for StarknetClient.get_repay_data method
+        :param mock_contract_from_address: unittest.mock.AsyncMock
+        :param deposit_token_addr: str
+        :param borrowing_token_addr: str
+        :return: None
+        """
+        sqrt_ratio = sum(
+            [ord(char) for char in deposit_token_addr + borrowing_token_addr]
+        )
+
+        mock_contract = AsyncMock()
+        mock_contract.functions["get_pool_price"].call = AsyncMock(
+            return_value=[
+                {
+                    "sqrt_ratio": sqrt_ratio,
+                }
+            ],
+        )
+        mock_contract_from_address.return_value = mock_contract
+
+        repay_data = await CLIENT.get_repay_data(
+            deposit_token_addr, borrowing_token_addr
+        )
+
+        mock_contract_from_address.assert_called_once()
+        mock_contract.functions["get_pool_price"].call.assert_called_once()
+
+        assert repay_data
+        assert isinstance(repay_data, dict)
+        assert {"supply_price", "debt_price", "pool_key"}.issubset(repay_data)
