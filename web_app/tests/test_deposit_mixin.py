fd2a686d60c2a504a76fbffdac9ec667b7c1d64e
vitaliypopel <vitaliypopel14@gmail.com>
2024-10-24 11:27:37 +0300

    Added mockers for TestDepositMixin tests

--- web_app/tests/test_deposit_mixin.py
+++ web_app/tests/test_deposit_mixin.py
@@ -1,5 +1,7 @@
 import pytest
 
+from unittest.mock import AsyncMock, MagicMock, patch
+
 from web_app.contract_tools.mixins.deposit import DepositMixin
 
 
@@ -17,8 +19,15 @@ class TestDepositMixin:
             ),
         ],
     )
+    @patch(
+        target="web_app.contract_tools.mixins.deposit.CLIENT.get_loop_liquidity_data",
+        new_callable=AsyncMock,
+    )
+    @patch("web_app.contract_tools.mixins.deposit.TokenParams.get_token_address")
     async def test_get_transaction_data(
         self,
+        mock_get_loop_liquidity_data: AsyncMock,
+        mock_get_token_address: MagicMock,
         deposit_token: str,
         amount: str,
         multiplier: int,
@@ -33,4 +42,14 @@ class TestDepositMixin:
             ("STRK",),
         ],
     )
-    async def test_get_repay_data(self, supply_token: str) -> None: ...
+    @patch(
+        target="web_app.contract_tools.mixins.deposit.CLIENT.get_repay_data",
+        new_callable=AsyncMock,
+    )
+    @patch("web_app.contract_tools.mixins.deposit.TokenParams.get_token_address")
+    async def test_get_repay_data(
+        self,
+        mock_get_relay_data: AsyncMock,
+        mock_get_token_address: MagicMock,
+        supply_token: str,
+    ) -> None: ...
