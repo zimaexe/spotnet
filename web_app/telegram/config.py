0ae8aca75fab83227bd96975f632aab033677d78
Teri-anric <2005ahi2005@gmail.com>
2024-10-27 10:41:34 +0000

    fix unseet WEBAPP_URL and docstring

--- web_app/telegram/config.py
+++ web_app/telegram/config.py
@@ -11,4 +11,4 @@ load_dotenv()
 
 # Retrieve the Telegram bot token from environment variables
 TELEGRAM_TOKEN = getenv("TELEGRAM_TOKEN")
-WEBAPP_URL = getenv("TELEGRAM_WEBAPP_URL")
+WEBAPP_URL = getenv("TELEGRAM_WEBAPP_URL", "spotnet.xyz")
