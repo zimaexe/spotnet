960dd0c0c62148d0a38de8c423426449dd51668f
vitaliypopel <vitaliypopel14@gmail.com>
2024-10-27 17:41:30 +0200

    Added "poetry run" before runnings commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 echo "Run migration with alembic"
-alembic -c web_app/alembic.ini upgrade head
+poetry run alembic -c web_app/alembic.ini upgrade head
 
 echo "Starting the server ..."
-
-uvicorn web_app.api.main:app --host 0.0.0.0 --port 8000
+poetry run uvicorn web_app.api.main:app --host 0.0.0.0 --port 8000
