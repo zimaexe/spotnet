55c07590b30059d907e54ea062916c526bd53880
Andrii Bogomolov <bogomolov.andrii@gmail.com>
2024-11-08 16:18:00 +0100

    fix/don't miss and information css
    Fixed styles for Don't miss out and Information pages

--- frontend/src/pages/spotnet/information/Information.jsx
+++ frontend/src/pages/spotnet/information/Information.jsx
@@ -1,6 +1,6 @@
 import './information.css';
-import React, { useEffect, useState } from "react";
-import StarMaker from "../../../components/StarMaker"; 
+import React, { useEffect, useState } from 'react';
+import StarMaker from '../../../components/StarMaker';
 import { axiosInstance } from 'utils/axios';
 
 const Information = () => {
@@ -8,27 +8,27 @@ const Information = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axiosInstance.get(`/api/get_stats`);
-                setData({
-                    total_opened_amount: response.data.total_opened_amount,
-                    unique_users: response.data.unique_users,
-                });
-            } catch (error) {
-                setError(error.response ? error.response.data.message : error.message);
-            } finally {
-                setLoading(false);
-            }
-        };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axiosInstance.get(`/api/get_stats`);
+        setData({
+          total_opened_amount: response.data.total_opened_amount,
+          unique_users: response.data.unique_users,
+        });
+      } catch (error) {
+        setError(error.response ? error.response.data.message : error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-        fetchData();
-    }, []);
+    fetchData();
+  }, []);
 
   const starData = [
     { top: 9, left: -6.2, size: 20 },
-    { top: 9, left: 39, size: 15 },
+    { top: 30, left: 39, size: 15 },
     { top: -8, left: 85, size: 18 },
     { top: 74, left: 43, size: 22 },
   ];
@@ -54,7 +54,9 @@ const Information = () => {
       <div className="card-info__container">
         <div className="card-info flex">
           <h1>TVL</h1>
-          <h3>{loading ? 'Loading...' : error ? `Error: ${error}` : formatCurrency(data.total_opened_amount)}</h3>
+          <h3 className={loading ? 'loading-text' : ''}>
+            {loading ? 'Loading...' : error ? `Error: ${error}` : formatCurrency(data.total_opened_amount)}
+          </h3>
         </div>
         <div className="infos">
           <div className="card-gradient"></div>
@@ -62,7 +64,9 @@ const Information = () => {
         </div>
         <div className="card-info flex">
           <h1>Users</h1>
-          <h3>{loading ? 'Loading...' : error ? `Error: ${error}` : data.unique_users}</h3>
+          <h3 className={loading ? 'loading-text' : ''}>
+            {loading ? 'Loading...' : error ? `Error: ${error}` : data.unique_users}
+          </h3>
         </div>
         <StarMaker starData={starData} />
       </div>
