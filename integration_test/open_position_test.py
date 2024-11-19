4823a00127719033e75781060aa8e6afa7350af0
bosun-kali <joshuaibitoye111@gmail.com>
2024-11-19 20:13:42 +0100

    draft3 test

--- integration_test/open_position_test.py
+++ integration_test/open_position_test.py
@@ -6,7 +6,6 @@ from web_app.api.main import app
 from web_app.db.crud import PositionDBConnector
 from web_app.contract_tools.mixins.deposit import DepositMixin
 from web_app.contract_tools.mixins.dashboard import DashboardMixin
-from web_app.db.models import Position
 
 client = TestClient(app)
 position_db = PositionDBConnector()
@@ -24,7 +23,7 @@ class PositionCreationTest:
     5. Update position status and validate changes.
     """
 
-    mock_position_data: Dict[str, Any] = {
+    mock_user_1: Dict[str, Any] = {
         "wallet_id": "0x011F0c180b9EbB2B3F9601c41d65AcA110E48aec0292c778f41Ae286C78Cc374",
         "token_symbol": "STRK",
         "amount": "2",
@@ -32,39 +31,49 @@ class PositionCreationTest:
         "borrowing_token": "STRK",
     }
 
-    @pytest.fixture
-    def setup_mock_user(self) -> Dict[str, Any]:
-        """
-        Create a mock user and position data for testing.
+    mock_user_2: Dict[str, Any] = {
+        "wallet_id": "id",
+        "token_symbol": "ETH",
+        "amount": "5",
+        "multiplier": 1,
+        "borrowing_token": "STRK",
+    }
 
-        Returns:
-            Dict[str, Any]: A dictionary containing position data.
-        """
-        position_data = self.mock_position_data
-        return {"position_data": position_data}
+    mock_user_3: Dict[str, Any] = {
+        "wallet_id": "id",
+        "token_symbol": "USDC",
+        "amount": "1",
+        "multiplier": 1,
+        "borrowing_token": "ETH",
+    }
 
-    def test_create_position(self, setup_mock_user: Dict[str, Any]) -> Position:
+    @pytest.mark.parametrize("mock_user", [(mock_user_1), (mock_user_2), (mock_user_3)])
+    def test_create_position(self, mock_user: Dict[str, Any]) -> None:
         """
-        Test creating a position and validating attributes.
-
         Args:
-            setup_mock_user (Dict[str, Any]): position data.
+            mock_user (Dict[str, Any]): position data.
 
         Returns:
-            Position
+            None
         """
+        # Extract position data
+        wallet_id = mock_user["wallet_id"]
+        token_symbol = mock_user["token_symbol"]
+        amount = mock_user["amount"]
+        multiplier = mock_user["multiplier"]
+        borrowing_token = mock_user["borrowing_token"]
+
+        position_db.create_user(wallet_id)
 
         transaction_data = DepositMixin.get_transaction_data(
-            deposit_token=setup_mock_user["position_data"]["token_symbol"],
-            amount=setup_mock_user["position_data"]["amount"],
-            multiplier=setup_mock_user["position_data"]["multiplier"],
-            wallet_id=setup_mock_user["user"]["wallet_id"],
-            borrowing_token=setup_mock_user["position_data"]["borrowing_token"],
+            deposit_token=token_symbol,
+            amount=amount,
+            multiplier=multiplier,
+            wallet_id=wallet_id,
+            borrowing_token=borrowing_token,
         )
         assert "caller" in transaction_data, "Transaction data missing 'caller'"
-        assert (
-            transaction_data["caller"] == setup_mock_user["user"]["wallet_id"]
-        ), "Mismatch in 'caller'"
+        assert transaction_data["caller"] == wallet_id, "Mismatch in 'caller'"
         assert (
             "pool_price" in transaction_data
         ), "'pool_price' missing in transaction data"
@@ -74,10 +83,10 @@ class PositionCreationTest:
         ), "'deposit_data' missing in transaction data"
 
         position = position_db.create_position(
-            wallet_id=setup_mock_user["user"]["wallet_id"],
-            token_symbol=setup_mock_user["position_data"]["token_symbol"],
-            amount=setup_mock_user["position_data"]["amount"],
-            multiplier=setup_mock_user["position_data"]["multiplier"],
+            wallet_id=wallet_id,
+            token_symbol=token_symbol,
+            amount=amount,
+            multiplier=multiplier,
         )
         assert (
             position.status == "pending"
@@ -90,38 +99,15 @@ class PositionCreationTest:
         position.start_price = current_prices[position.token_symbol]
         position.created_at = datetime.utcnow()
 
-        position_db.update_position(position, position.amount, position.multiplier)
-
         print(
             f"Position {position.id} created successfully with status '{position.status}'."
         )
-        return position
 
-    def test_open_position(self, position_id: str) -> None:
-        """
-        Test opening a position and validating status updates.
-
-        Args:
-            position id : str
-
-        Returns:
-            None
-        """
-        response = client.get(f"/api/open-position?position_id={position_id}")
-        assert response.status_code == 200, "Opening position failed"
+        # Open position
+        position_db.open_position(position.id, current_prices)
         assert (
-            response.json().get("status") == "opened"
-        ), "Position status mismatch after opening"
+            position.status == "opened"
+        ), "Position status should be 'opened' upon creation"
+        print(f"Position {position.id} successfully opened.")
 
-        print(f"Position {position_id} successfully opened.")
-
-    def test_full_workflow(self) -> None:
-        """
-        Comprehensive test for the position workflow.
-
-        Returns:
-            None
-        """
-        position = self.test_create_position(self.setup_mock_user)
-
-        self.test_open_position(position.id)
+        position_db.delete_position(position)
