48f13bfc208c1c80e2e66656dae24761829ec938
KevinLatino <latinokevin9@gmail.com>
2024-11-19 23:01:42 -0600

    final version with the new navbar

--- frontend/src/pages/spotnet/dashboard/Dashboard.jsx
+++ frontend/src/pages/spotnet/dashboard/Dashboard.jsx
@@ -104,141 +104,146 @@ export default function Component({ walletId }) {
 
   return (
     <div className="dashboard-wrapper">
-      {isLoading && <Spinner loading={isLoading} />}
       <div className="dashboard-container">
+        {isLoading && <Spinner loading={isLoading} />}
         <h1 className="dashboard-title">zkLend Position</h1>
-
-        <div className="top-cards">
-          <div className="card">
-            <div className="card-header">
-              <HealthIcon className="icon" />
-              <span className="label">Health Factor</span>
-            </div>
-            <div className="card-value">
-              <span className="top-card-value">
-                {healthFactor}
-              </span>
-            </div>
-          </div>
-
-          <div className="card">
-            <div className="card-header">
-              <EthIcon className="icon" />
-              <span className="label">Borrow Balance</span>
-            </div>
-            <div className="card-value">
-              <span className="currency-symbol">$</span>
-              <span className="top-card-value">
-                {cardData[1].balance}
-              </span>
-            </div>
-          </div>
-        </div>
-
-        <div className="main-card">
-          <div className="tabs">
-            <button
-              onClick={() => setIsCollateralActive(true)}
-              className={`tab ${isCollateralActive ? "active" : ""}`}
-            >
-              <CollateralIcon className="tab-icon" />
-              Collateral & Earnings
-            </button>
-
-            <div className="tab-divider" />
-
-            <button
-              onClick={() => setIsCollateralActive(false)}
-              className={`tab ${!isCollateralActive ? "active borrow" : ""}`}
-            >
-              <BorrowIcon className="tab-icon" />
-              Borrow
-            </button>
-            <div className="tab-indicator-container">
-              <div
-                className={`tab-indicator ${isCollateralActive ? "collateral" : "borrow"}`}
-              />
-            </div>
-          </div>
-
-          {isCollateralActive ? (
-            <div className="tab-content">
-              <div className="balance-info">
-                <div className="currency-info">
-                  {React.createElement(cardData[0].currencyIcon, { className: "icon" })}
-                  <span className="currency-name">{cardData[0].currencyName}</span>
-                </div>
-                <span>
-                  <span className="balance-label">Balance: </span>
-                  <span className="balance-value">
-                    {cardData[0].balance}
-                  </span>
-                </span>
-                <span>
-                  <span className="balance-label">Start sum: </span>
-                  <span className="balance-value">
-                    <span className="currency-symbol">$</span>
-                    {startSum}
-                  </span>
-                </span>
-                <span>
-                  <span className="balance-label">Current sum: </span>
-                  <span
-                    className={
-                      currentSum === 0 ? "current-sum-white" : getCurrentSumColor()
-                    }
-                  >
-                    <span className="currency-symbol">$</span>
-                    {currentSum}
-                    {currentSum > startSum && currentSum !== 0 && (
-                      <TrendingUp
-                        color="#60AF77"
-                        size={23}
-                        style={{ marginLeft: "8px" }}
-                      />
-                    )}
-                    {currentSum < startSum && currentSum !== 0 && (
-                      <TrendingDown
-                        color="#F42222"
-                        size={22}
-                        style={{ marginLeft: "8px" }}
-                      />
-                    )}
-                  </span>
+        <div className="main-container">
+          <div className="top-cards">
+            <div className="card">
+              <div className="card-header">
+                <HealthIcon className="icon" />
+                <span className="label">Health Factor</span>
+              </div>
+              <div className="card-value">
+                <span className="top-card-value">
+                  {healthFactor}
                 </span>
               </div>
             </div>
-          ) : (
-            <div className="tab-content">
-              <div className="balance-info">
-                <div className="currency-info">
-                  {React.createElement(cardData[1].currencyIcon, { className: "icon" })}
-                  <span className="currency-name">{cardData[1].currencyName}</span>
-                </div>
-                <span>
-                  <span className="balance-label">Balance: </span>
-                  <span className="balance-value">
-                    {cardData[1].balance}
-                  </span>
+
+            <div className="card">
+              <div className="card-header">
+                <EthIcon className="icon" />
+                <span className="label">Borrow Balance</span>
+              </div>
+              <div className="card-value">
+                <span className="currency-symbol">$</span>
+                <span className="top-card-value">
+                  {cardData[1].balance}
                 </span>
               </div>
             </div>
-          )}
+          </div>
+
+          <div className="main-card">
+            <div className="tabs">
+              <button
+                onClick={() => setIsCollateralActive(true)}
+                className={`tab ${isCollateralActive ? "active" : ""}`}
+              >
+                <CollateralIcon className="tab-icon" />
+                <span className="tab-title">
+                  Collateral & Earnings
+                </span>
+              </button>
+
+              <div className="tab-divider" />
+
+              <button
+                onClick={() => setIsCollateralActive(false)}
+                className={`tab ${!isCollateralActive ? "active borrow" : ""}`}
+              >
+                <BorrowIcon className="tab-icon" />
+                <span className="tab-title">
+                  Borrow
+                </span>
+              </button>
+              <div className="tab-indicator-container">
+                <div
+                  className={`tab-indicator ${isCollateralActive ? "collateral" : "borrow"}`}
+                />
+              </div>
+            </div>
+
+            {isCollateralActive ? (
+              <div className="tab-content">
+                <div className="balance-info">
+                  <div className="currency-info">
+                    {React.createElement(cardData[0].currencyIcon, { className: "icon" })}
+                    <span className="currency-name">{cardData[0].currencyName}</span>
+                  </div>
+                  <span>
+                    <span className="balance-label">Balance: </span>
+                    <span className="balance-value">
+                      {cardData[0].balance}
+                    </span>
+                  </span>
+                  <span>
+                    <span className="balance-label">Start sum: </span>
+                    <span className="balance-value">
+                      <span className="currency-symbol">$</span>
+                      {startSum}
+                    </span>
+                  </span>
+                  <span>
+                    <span className="balance-label">Current sum: </span>
+                    <span
+                      className={
+                        currentSum === 0 ? "current-sum-white" : getCurrentSumColor()
+                      }
+                    >
+                      <span className="currency-symbol">$</span>
+                      {currentSum}
+                      {currentSum > startSum && currentSum !== 0 && (
+                        <TrendingUp
+                          color="#60AF77"
+                          size={23}
+                          style={{ marginLeft: "8px" }}
+                        />
+                      )}
+                      {currentSum < startSum && currentSum !== 0 && (
+                        <TrendingDown
+                          color="#F42222"
+                          size={22}
+                          style={{ marginLeft: "8px" }}
+                        />
+                      )}
+                    </span>
+                  </span>
+                </div>
+              </div>
+            ) : (
+              <div className="tab-content">
+                <div className="balance-info">
+                  <div className="currency-info">
+                    {React.createElement(cardData[1].currencyIcon, { className: "icon" })}
+                    <span className="currency-name">{cardData[1].currencyName}</span>
+                  </div>
+                  <span>
+                    <span className="balance-label">Balance: </span>
+                    <span className="balance-value">
+                      {cardData[1].balance}
+                    </span>
+                  </span>
+                </div>
+              </div>
+            )}
+          </div>
+
+          <button
+            className="redeem-button"
+            onClick={() => closePositionEvent()}
+            disabled={isClosing}
+          >
+            {isClosing ? "Closing..." : "Redeem"}
+          </button>
+          {closePositionError && <div>Error: {closePositionError.message}</div>}
+
+          <button className="telegram-button">
+            <TelegramIcon className="tab-icon" />
+            Enable telegram notification bot
+          </button>
         </div>
-
-        <button
-          className="redeem-button"
-          onClick={() => closePositionEvent()}
-          disabled={isClosing}
-        >
-          {isClosing ? "Closing..." : "Redeem"}
-        </button>
-        {closePositionError && <div>Error: {closePositionError.message}</div>}
-
-        <button className="telegram-button">
-          <TelegramIcon className="tab-icon" />
-          Enable telegram notification bot
-        </button>
       </div>
     </div>
   );
