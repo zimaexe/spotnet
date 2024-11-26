1fa5a7ac53db107cab9de4fca7823fe631d8ffc1
Ekuma Matthew <79433184+ekumamatthew@users.noreply.github.com>
2024-11-26 21:14:10 +0000

    feat/issue 242 ui fix

--- frontend/src/components/modals/ClosePositionModal.jsx
+++ frontend/src/components/modals/ClosePositionModal.jsx
@@ -3,21 +3,22 @@ import './ClosePositionModal.css';
 const ClosePositionModal = ({ isOpen, onClose, closePosition, text, header, actionText }) => {
   if (!isOpen) return null;
   return (
-    <div className="overlay">
-      <div className="backdrop" onClick={onClose} />
-      <div className="container">
-        <div className="content">
-          <div className="header">{header}</div>
+    <div onClick={onClose} className="overlay">
+      <div className="background">
+        <div className="container">
+          <div className="content">
+            <div className="header">{header}</div>
 
-          <div className="popup-body">
-            <h3>{actionText}</h3>
-            <p>{text}</p>
+            <div className="popup-body">
+              <h3>{actionText}</h3>
+              <p>{text}</p>
+            </div>
           </div>
-
           <div className="popup-buttons">
             <button onClick={onClose} className="popup-button popup-button-cancel">
-              Cancel
+              <div className="popup-button-cancel-text">Cancel</div>
             </button>
+
             <button onClick={closePosition} className="popup-button popup-button-close">
               Close Active Position
             </button>
