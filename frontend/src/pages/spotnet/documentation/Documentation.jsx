bcd98a67e74ddee22965c0f6b5110f6018369846
jahrulezfrancis <longsaarmuknaan@gmail.com>
2024-11-28 23:33:14 +0100

    Updating tableOfContentProps

--- frontend/src/pages/spotnet/documentation/Documentation.jsx
+++ frontend/src/pages/spotnet/documentation/Documentation.jsx
@@ -89,7 +89,7 @@ const Documentation = () => {
 
   return (
     <div className="documentation-page">
-      <TableOfContents items={tableOfContents} />
+      <TableOfContents defaultActiveId={"introduction"} headerHeight={80} items={tableOfContents} />
 
       <div className="main-content">
         <h1 className="main-title">zkLend Documentation</h1>
