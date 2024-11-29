dcfa1623ea6ccb092b21c3101c5ab51d9474ea6f
gregemax <140010035+gregemax@users.noreply.github.com>
2024-11-29 18:46:15 +0000

    changes

--- frontend/src/pages/vault/stake/Stake.jsx
+++ frontend/src/pages/vault/stake/Stake.jsx
@@ -6,28 +6,24 @@ import { ReactComponent as DAI } from '../../../assets/icons/dai.svg';
 import './stake.css';
 import { VaultLayout } from '../VaultLayout';
 import { Button } from 'components/ui/Button';
-import MetricCard from 'components/StakeCard/MetricCard';
+//import MetricCard from 'components/StakeCard/MetricCard';
 import GasFee from 'components/GasFee/GasFee';
 import BalanceCards from 'components/BalanceCards';
-
-
 
 function Stake() {
   const [selectedNetwork, setSelectedNetwork] = useState('Starknet');
   const [amount, setAmount] = useState('0');
-
   const [balances, setBalances] = useState([
     { icon: <STRK />, title: 'STRK', balance: '0.046731' },
     { icon: <USDC />, title: 'APY', balance: '0.046731' },
-    { icon: <ETH />, title: 'ETH', balance: '0.00' },
-    { icon: <DAI />, title: 'DAI', balance: '0.00' },
+    { icon: <ETH />, title: 'ETH', balance: '0.046731' },
+    { icon: <DAI />, title: 'DAI', balance: '0.046731' },
   ]);
-
 
   const networks = [{ name: 'Starknet', image: STRK }];
 
-  const handleChange = (e) => {
-    setSelectedNetwork(e.target.value);
+  const handleChange = (network) => {
+    setSelectedNetwork(network.name);
   };
 
   const handleAmountChange = (e) => {
@@ -39,170 +35,127 @@ function Stake() {
   };
 
   return (
-    <>
-      <div className="mobile-vault-container">
-        <div className="form-content-wrapper">
+    <div className="stake-page">
+      <VaultLayout className="desktop-layout">
+        <StakeWrapper 
+          selectedNetwork={selectedNetwork}
+          networks={networks}
+          amount={amount}
+          balances={balances}
+          setBalances={setBalances}
+          handleChange={handleChange}
+          handleAmountChange={handleAmountChange}
+        />
+      </VaultLayout>
+      
+      <div className="mobile-layout">
+        <StakeWrapper 
+          selectedNetwork={selectedNetwork}
+          networks={networks}
+          amount={amount}
+          balances={balances}
+          setBalances={setBalances}
+          handleChange={handleChange}
+          handleAmountChange={handleAmountChange}
+        />
+      </div>
+    </div>
+  );
+}
 
-
-          {/* Balance Cards */}
-          <div className='mobile-balances-container '>
-            <div className='mobile-balances-scroller '>
-              <BalanceCards balances={balances} setBalances={setBalances} walletId={null} />
-            </div>
+function StakeWrapper({ 
+  selectedNetwork, 
+  networks, 
+  amount, 
+  balances,
+  setBalances,
+  handleChange, 
+  handleAmountChange 
+}) {
+  return (
+    <div className="form-content-wrapper">
+        <div className="stake-container">
+        <div className='mobile-balances-container '>
+             <div className='mobile-balances-scroller '>
+               <BalanceCards 
+                 balances={balances} 
+                setBalances={setBalances} 
+                 walletId={null} 
+              />
+             </div>
           </div>
+          {/* <div className='main-container'>
+            <div className="top-cards">
+              <MetricCard title="STRK Balance" value="0.046731" icon={STRK} />
+              <MetricCard title="APY Balance" value="0.046731" icon={USDC} />
+            </div>
+          </div> */}
 
-          <h1 className="mobile-stake-title ">Please submit your stake</h1>
-
-          {/* Staking Section */}
-          <div className='mobile-border-container'>
-            <div className="main-card">
-              <div className="network-selector-container">
-                <div className="network-selector">
-                  <div className="selected-network">
-                    <img
-                      src={networks.find((network) => network.name === selectedNetwork)?.image}
-                      alt={selectedNetwork}
-                      className="network-icon"
-                    />
-                    <span>{selectedNetwork}</span>
-                  </div>
-                  <svg
-                    className="chevron"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M6 9L12 15L18 9"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
-                </div>
-
-                <div className="network-dropdown">
-                  {networks.map((network) => (
-                    <div key={network.name} className="network-option" onClick={() => handleChange(network)}>
-                      <img src={network.image} alt={network.name} className="network-icon" />
-                      <span>{network.name}</span>
-                    </div>
-                  ))}
-                </div>
-              </div>
-              <div className="currency-container">
-
-                <div className="amount-input" aria-labelledby="amount-input-label">
-                  <input
-                    type="text"
-                    id="amount-field"
-                    value={amount}
-                    onChange={handleAmountChange}
-                    pattern="^\d*\.?\d*$"
-                    className="amount-field"
-                    aria-describedby="currency-symbol"
-                    placeholder="0.00"
+          <h1 className="stake-title">Please submit your leverage details</h1>
+          <div className="main-card">
+            <div className="network-selector-container">
+              <div className="network-selector">
+                <div className="selected-network">
+                  <img
+                    src={networks.find((network) => network.name === selectedNetwork)?.image}
+                    alt={selectedNetwork}
+                    className="network-icon"
                   />
-                  <span id="currency-symbol" className="mobile-currency">STRK</span>
+                  <span>{selectedNetwork}</span>
                 </div>
-
-                <div className="apy-rate">$0.00 APY / year</div>
+                <svg
+                  className="chevron"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M6 9L12 15L18 9"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
               </div>
-              <GasFee />
+
+              <div className="network-dropdown">
+                {networks.map((network) => (
+                  <div key={network.name} className="network-option" onClick={() => handleChange(network)}>
+                    <img src={network.image} alt={network.name} className="network-icon" />
+                    <span>{network.name}</span>
+                  </div>
+                ))}
+              </div>
             </div>
+
+            <div className="amount-input" aria-labelledby="amount-input-label">
+              <input
+                type="text"
+                id="amount-field"
+                value={amount}
+                onChange={handleAmountChange}
+                pattern="^\d*\.?\d*$"
+                className="amount-field"
+                aria-describedby="currency-symbol"
+                placeholder="0.00"
+              />
+              <span id="currency-symbol" className="currency">STRK</span>
+            </div>
+
+            <div className="apy-rate">$0.00 APY / year</div>
+            <GasFee />
           </div>
 
-          <div className='parent-btn-container '>
-            <Button variant="secondary" size="lg" className="mobile-stake-button">
-              Stake
-            </Button>
-          </div>
+          <Button variant="secondary" size="lg" className="stake-button">
+            Stake
+          </Button>
         </div>
       </div>
-
-      <div className="vault-container">
-
-        <VaultLayout>
-          <div className="stake-wrapper">
-            <div className="stake-container">
-              <div className='main-container'>
-                <div className="top-cards">
-                  <MetricCard title="STRK Balance" value="0.046731" icon={STRK} />
-                  <MetricCard title="APY Balance" value="0.046731" icon={USDC} />
-                </div>
-              </div>
-
-              <h1 className="stake-title">Please submit your leverage details</h1>
-
-              <div className="main-card">
-                <div className="network-selector-container">
-                  <div className="network-selector">
-                    <div className="selected-network">
-                      <img
-                        src={networks.find((network) => network.name === selectedNetwork)?.image}
-                        alt={selectedNetwork}
-                        className="network-icon"
-                      />
-                      <span>{selectedNetwork}</span>
-                    </div>
-                    <svg
-                      className="chevron"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M6 9L12 15L18 9"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      />
-                    </svg>
-                  </div>
-
-                  <div className="network-dropdown">
-                    {networks.map((network) => (
-                      <div key={network.name} className="network-option" onClick={() => handleChange(network)}>
-                        <img src={network.image} alt={network.name} className="network-icon" />
-                        <span>{network.name}</span>
-                      </div>
-                    ))}
-                  </div>
-                </div>
-
-                <div className="amount-input" aria-labelledby="amount-input-label">
-                  <input
-                    type="text"
-                    id="amount-field"
-                    value={amount}
-                    onChange={handleAmountChange}
-                    pattern="^\d*\.?\d*$"
-                    className="amount-field"
-                    aria-describedby="currency-symbol"
-                    placeholder="0.00"
-                  />
-                  <span id="currency-symbol" className="currency">STRK</span>
-                </div>
-
-                <div className="apy-rate">$0.00 APY / year</div>
-                <GasFee />
-              </div>
-
-              <Button variant="secondary" size="lg" className="stake-button">
-                Stake
-              </Button>
-            </div>
-          </div>
-        </VaultLayout>
-      </div>
-
-    </>
-  )
+   
+  );
 }
+
 export default Stake;
