5f7e48b3e0941d14d6ce139d33efbde94e88592f
djeck1432 <oleh.kostashchuk@gmail.com>
2024-12-07 13:43:01 +0100

    uncomment

--- web_app/tests/test_deposit_mixin.py
+++ web_app/tests/test_deposit_mixin.py
@@ -7,124 +7,120 @@ from unittest.mock import AsyncMock, MagicMock, patch
 
 from web_app.contract_tools.mixins.deposit import DepositMixin
 
-# https://starkscan.co/contract/0x04c0a5193d58f74fbace4b74dcf65481e734ed1714121bdc571da345540efa05#read-write-contract
-# get_user_debt_for_token(user_contract, token)
+class TestDepositMixin:
+    """
+    Test cases for web_app.contract_tools.mixins.deposit.DepositMixin class
+    """
 
-# amount * collateral_factor /(borrow_factor * borrow_amount)
-# class TestDepositMixin:
-#     """
-#     Test cases for web_app.contract_tools.mixins.deposit.DepositMixin class
-#     """
-#
-#     @pytest.mark.asyncio
-#     @pytest.mark.parametrize(
-#         "deposit_token, amount, multiplier, wallet_id, borrowing_token",
-#         [
-#             (
-#                 "STRK",
-#                 "100_000",
-#                 2,
-#                 "0x27994c503bd8c32525fbdaf9d398bdd4e86757988c64581b055a06c5955ea49",
-#                 "STRK",
-#             ),
-#             (
-#                 "USDC",
-#                 "3333.3",
-#                 4,
-#                 "0x27994c503bd8c32525fbdaf9d398bdd4e86757988c64581b055a06c5955ea49",
-#                 "ETH",
-#             ),
-#             (
-#                 "",
-#                 "0",
-#                 0,
-#                 "invalid_wallet_id",
-#                 "",
-#             ),
-#         ],
-#     )
-#     @patch(
-#         target="web_app.contract_tools.mixins.deposit.CLIENT.get_loop_liquidity_data",
-#         new_callable=AsyncMock,
-#     )
-#     @patch("web_app.contract_tools.mixins.deposit.TokenParams.get_token_address")
-#     async def test_get_transaction_data(
-#         self,
-#         mock_get_loop_liquidity_data: AsyncMock,
-#         deposit_token: str,
-#         amount: str,
-#         multiplier: int,
-#         wallet_id: str,
-#         borrowing_token: str,
-#     ) -> None:
-#         """
-#         Tests cases for DepositMixin.get_transaction_data method
-#         :param mock_get_loop_liquidity_data: unittest.mock.AsyncMock
-#         :param deposit_token: Deposit token
-#         :param amount: Amount to deposit
-#         :param multiplier: Multiplier
-#         :param wallet_id: Wallet ID
-#         :param borrowing_token: Borrowing token
-#         :return: None
-#         """
-#
-#         expected_transaction_data = {
-#             "caller": wallet_id,
-#             "pool_price": "mocked_pool_price",
-#             "pool_key": "mocked_pool_key",
-#             "deposit_data": "mocked_deposit_data",
-#         }
-#
-#         mock_get_loop_liquidity_data.return_value = expected_transaction_data
-#
-#         transaction_data = await DepositMixin.get_transaction_data(
-#             deposit_token, amount, multiplier, wallet_id, borrowing_token
-#         )
-#
-#         mock_get_loop_liquidity_data.assert_called_once_with(
-#             deposit_token,
-#             int(Decimal(amount) * Decimal(10**18)),
-#             multiplier,
-#             wallet_id,
-#             borrowing_token,
-#         )
-#
-#         assert transaction_data == expected_transaction_data
-#
-#     # @pytest.mark.asyncio
-#     # @pytest.mark.parametrize(
-#     #     "supply_token",
-#     #     ["STRK", "ETH", "USDC", None, 3.14, {}],
-#     # )
-#     # @patch(
-#     #     target="web_app.contract_tools.mixins.deposit.CLIENT.get_repay_data",
-#     #     new_callable=AsyncMock,
-#     # )
-#     # async def test_get_repay_data(
-#     #     self,
-#     #     mock_get_repay_data: AsyncMock,
-#     #     supply_token: str,
-#     # ) -> None:
-#     #     """
-#     #     Test cases for DepositMixin.get_repay_data method
-#     #     :param mock_get_repay_data: unittest.mock.AsyncMock
-#     #     :param supply_token: Deposit token
-#     #     :return: None
-#     #     """
-#     #     expected_repay_data = {
-#     #         "supply_token": supply_token,
-#     #         "debt_token": "USDC",
-#     #         "supply_price": "mocked_supply_price",
-#     #         "debt_price": "mocked_debt_price",
-#     #         "pool_key": "mocked_pool_key",
-#     #     }
-#     #
-#     #     mock_get_repay_data.return_value = {
-#     #         "supply_price": expected_repay_data["supply_price"],
-#     #         "debt_price": expected_repay_data["debt_price"],
-#     #         "pool_key": expected_repay_data["pool_key"],
-#     #     }
-#     #
-#     #     repay_data = await DepositMixin.get_repay_data(supply_token)
-#     #
-#     #     assert repay_data == expected_repay_data
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize(
+        "deposit_token, amount, multiplier, wallet_id, borrowing_token",
+        [
+            (
+                "STRK",
+                "100_000",
+                2,
+                "0x27994c503bd8c32525fbdaf9d398bdd4e86757988c64581b055a06c5955ea49",
+                "STRK",
+            ),
+            (
+                "USDC",
+                "3333.3",
+                4,
+                "0x27994c503bd8c32525fbdaf9d398bdd4e86757988c64581b055a06c5955ea49",
+                "ETH",
+            ),
+            (
+                "",
+                "0",
+                0,
+                "invalid_wallet_id",
+                "",
+            ),
+        ],
+    )
+    @patch(
+        target="web_app.contract_tools.mixins.deposit.CLIENT.get_loop_liquidity_data",
+        new_callable=AsyncMock,
+    )
+    @patch("web_app.contract_tools.mixins.deposit.TokenParams.get_token_address")
+    async def test_get_transaction_data(
+        self,
+        mock_get_loop_liquidity_data: AsyncMock,
+        deposit_token: str,
+        amount: str,
+        multiplier: int,
+        wallet_id: str,
+        borrowing_token: str,
+    ) -> None:
+        """
+        Tests cases for DepositMixin.get_transaction_data method
+        :param mock_get_loop_liquidity_data: unittest.mock.AsyncMock
+        :param deposit_token: Deposit token
+        :param amount: Amount to deposit
+        :param multiplier: Multiplier
+        :param wallet_id: Wallet ID
+        :param borrowing_token: Borrowing token
+        :return: None
+        """
+
+        expected_transaction_data = {
+            "caller": wallet_id,
+            "pool_price": "mocked_pool_price",
+            "pool_key": "mocked_pool_key",
+            "deposit_data": "mocked_deposit_data",
+        }
+
+        mock_get_loop_liquidity_data.return_value = expected_transaction_data
+
+        transaction_data = await DepositMixin.get_transaction_data(
+            deposit_token, amount, multiplier, wallet_id, borrowing_token
+        )
+
+        mock_get_loop_liquidity_data.assert_called_once_with(
+            deposit_token,
+            int(Decimal(amount) * Decimal(10**18)),
+            multiplier,
+            wallet_id,
+            borrowing_token,
+        )
+
+        assert transaction_data == expected_transaction_data
+
+    # @pytest.mark.asyncio
+    # @pytest.mark.parametrize(
+    #     "supply_token",
+    #     ["STRK", "ETH", "USDC", None, 3.14, {}],
+    # )
+    # @patch(
+    #     target="web_app.contract_tools.mixins.deposit.CLIENT.get_repay_data",
+    #     new_callable=AsyncMock,
+    # )
+    # async def test_get_repay_data(
+    #     self,
+    #     mock_get_repay_data: AsyncMock,
+    #     supply_token: str,
+    # ) -> None:
+    #     """
+    #     Test cases for DepositMixin.get_repay_data method
+    #     :param mock_get_repay_data: unittest.mock.AsyncMock
+    #     :param supply_token: Deposit token
+    #     :return: None
+    #     """
+    #     expected_repay_data = {
+    #         "supply_token": supply_token,
+    #         "debt_token": "USDC",
+    #         "supply_price": "mocked_supply_price",
+    #         "debt_price": "mocked_debt_price",
+    #         "pool_key": "mocked_pool_key",
+    #     }
+    #
+    #     mock_get_repay_data.return_value = {
+    #         "supply_price": expected_repay_data["supply_price"],
+    #         "debt_price": expected_repay_data["debt_price"],
+    #         "pool_key": expected_repay_data["pool_key"],
+    #     }
+    #
+    #     repay_data = await DepositMixin.get_repay_data(supply_token)
+    #
+    #     assert repay_data == expected_repay_data
