c7c42daa7e978f6ce615353b2ac86b60dc0385b3
benard <benardsimon7@gmail.com>
2024-12-21 18:59:28 +0100

    refactor: move mobile background to a global implementation

--- frontend/src/pages/spotnet/dashboard/withdraw-all/WithdrawAll.jsx
+++ frontend/src/pages/spotnet/dashboard/withdraw-all/WithdrawAll.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import BalanceCards from 'components/ui/balance-cards/BalanceCards';
 import './withdraw_all.css';
+import '../../../../../../frontend/src/globals.css';
 import { useWalletStore } from 'stores/useWalletStore';
 import Button from 'components/ui/Button/Button';
 import useWithdrawAll from 'hooks/useWithdrawAll';
