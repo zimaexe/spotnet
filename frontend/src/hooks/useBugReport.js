05b9ad0098a3d90a7a9a4c430ba96c21d6e18b8c
vadim-ghostman <vadimtovtin@gmail.com>
2025-01-27 20:54:41 +0100

    removed generation of telegram link in bug report sending

--- frontend/src/hooks/useBugReport.js
+++ frontend/src/hooks/useBugReport.js
@@ -1,17 +1,11 @@
 import { useMutation } from '@tanstack/react-query';
 import { axiosInstance } from 'utils/axios';
-import { generateTelegramLink } from 'services/telegram';
+// import { generateTelegramLink } from 'services/telegram';
 import { notify } from 'components/layout/notifier/Notifier';
 
 export const useBugReport = (walletId, bugDescription, onClose) => {
   const mutation = useMutation({
     mutationFn: async () => {
-      if (!window?.Telegram?.WebApp?.initData?.user?.id) {
-        const { subscription_link } = await generateTelegramLink(walletId);
-        window.open(subscription_link, '_blank');
-        return;
-      }
-
       return await axiosInstance.post(`/api/save-bug-report`, {
         telegram_id: window?.Telegram?.WebApp?.initData?.user?.id,
         wallet_id: walletId,
