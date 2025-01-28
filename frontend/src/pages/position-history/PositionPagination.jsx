984aa1abfd3875d2306405816824fe43e0d01d61
Adamu Jethro <jethroadamzy@gmail.com>
2025-01-28 22:26:22 +0100

    refac : updated pagination

--- frontend/src/pages/position-history/PositionPagination.jsx
+++ frontend/src/pages/position-history/PositionPagination.jsx
@@ -3,12 +3,20 @@ import ArrowLeftIcon from '@/assets/icons/arrow-left.svg?react';
 import ArrowRightIcon from '@/assets/icons/arrow-right.svg?react';
 import './positionPagination.css';
 
-export default function PositionPagination({ currentPage, setCurrentPage, isPending, tableData, positionsOnPage }) {
-  const pagesCount = (totalItems, itemsPerPage) => Math.ceil(totalItems / itemsPerPage);
-  const range = (length, startIdx = 1) => [...Array(length).keys()].map((i) => i + startIdx);
+export default function PositionPagination({
+  currentPage,
+  setCurrentPage,
+  isPending,
+  tableData,
+  positionsOnPage,
+}) {
+  const totalItems = tableData?.total_count || 0;
+  const totalPages = Math.ceil(totalItems / positionsOnPage);
+  const range = (length, startIdx = 1) =>
+    [...Array(length).keys()].map((i) => i + startIdx);
 
   const setPage = (page) => {
-    if (isPending || page < 1 || page > pagesCount(tableData.length, positionsOnPage)) {
+    if (isPending || page < 1 || page > totalPages) {
       return;
     }
     setCurrentPage(page);
@@ -17,26 +25,30 @@ export default function PositionPagination({ currentPage, setCurrentPage, isPend
   return (
     <div className="position-pagination">
       <div
-        className={`pagination-button button-prev ${currentPage == 1 ? 'disabled' : ''}`}
+        className={`pagination-button button-prev ${
+          currentPage === 1 ? 'disabled' : ''
+        }`}
         onClick={() => setPage(currentPage - 1)}
       >
         <ArrowLeftIcon />
       </div>
       <div className="pagination-pages-container">
-        {!isPending && tableData
-          ? range(pagesCount(tableData.length, positionsOnPage)).map((page) => (
-              <div
-                className={`pagination-page-number ${currentPage == page ? 'page-selected' : ''}`}
-                key={page}
-                onClick={() => setPage(page)}
-              >
-                {page}
-              </div>
-            ))
-          : null}
+        {range(totalPages).map((page) => (
+          <div
+            className={`pagination-page-number ${
+              currentPage === page ? 'page-selected' : ''
+            }`}
+            key={page}
+            onClick={() => setPage(page)}
+          >
+            {page}
+          </div>
+        ))}
       </div>
       <div
-        className={`pagination-button button-next ${tableData && currentPage == pagesCount(tableData.length, positionsOnPage) ? 'disabled' : ''}`}
+        className={`pagination-button button-next ${
+          currentPage === totalPages ? 'disabled' : ''
+        }`}
         onClick={() => setPage(currentPage + 1)}
       >
         <ArrowRightIcon />
