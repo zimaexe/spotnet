7cee886f193bcf91a09b47a5c069286ae712db35
Ugochukwu Benedict Nebolisa <nebolisaugochukwu@gmail.com>
2025-01-30 17:40:44 +0100

    fix: changes requested

--- frontend/src/components/vault/gas-fee/GasFee.jsx
+++ frontend/src/components/vault/gas-fee/GasFee.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import SettingIcon from '@/assets/icons/settings.svg?react';
-import './gasFee.css';
 
 export default function GasFee() {
   return (
-    <div className="main-card-footer">
-      <div className="gas-fee-container w-full flex flex-row  items-center justify-between gap-2 py-3 px-2 border-t border-[#36294E] " style={{justifyContent: "space-between"}}>
-        <div className="gas-fee-circle">
-          <SettingIcon className="gas-fee-icon" />
-        </div>
-        <div className="gas-fee-title text-sm text-[#83919F] font-normal ">Gas fee: 0.00 STRK</div>
+    <div className="w-full flex flex-row  items-center justify-between gap-2 py-3 px-2 border-t border-light-purple">
+      <div className="bg-footer-divider rounded-full p-2">
+        <SettingIcon />
       </div>
+      <div className="text-sm text-[#83919F] font-normal ">Gas fee: 0.00 STRK</div>
     </div>
   );
 }
