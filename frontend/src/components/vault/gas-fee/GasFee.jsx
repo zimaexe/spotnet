09bdc7415c06320ceb3749fd7a7bbe9f1c8e9af5
chiscookeke11 <chiscookeke11@gmail.com>
2025-01-29 16:54:27 -1100

    fix: vault stake page

--- frontend/src/components/vault/gas-fee/GasFee.jsx
+++ frontend/src/components/vault/gas-fee/GasFee.jsx
@@ -5,11 +5,11 @@ import './gasFee.css';
 export default function GasFee() {
   return (
     <div className="main-card-footer">
-      <div className="gas-fee-container">
+      <div className="gas-fee-container w-full flex flex-row  items-center justify-between gap-2 py-4 px-2 border-t border-[#36294E] " style={{justifyContent: "space-between"}}>
         <div className="gas-fee-circle">
           <SettingIcon className="gas-fee-icon" />
         </div>
-        <div className="gas-fee-title">Gas fee: 0.00 STRK</div>
+        <div className="gas-fee-title text-sm text-[#83919F] font-normal ">Gas fee: 0.00 STRK</div>
       </div>
     </div>
   );
