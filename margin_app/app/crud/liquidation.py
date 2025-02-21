e1afe4387a05291b2a2aa18e2e5c272a74553640
KAMALDEEN333 <kamalaliyu503@gmail.com>
2025-02-21 16:34:04 +0100

    Update liquidation.py

--- margin_app/app/crud/liquidation.py
+++ margin_app/app/crud/liquidation.py
@@ -2,15 +2,16 @@
 
 from app.crud.base import DBConnector
 from app.models.liquidation import Liquidation
-from sqlalchemy.orm import Session
 
 class LiquidationCRUD(DBConnector):
     """Handles database operations for liquidations."""
 
-    def liquidate_position(self, db: Session, position_id: int):
-        """Liquidates a position based on the given position ID."""
+    async def liquidate_position(self, position_id: int) -> Liquidation:
+        """
+        Liquidates a position by creating a liquidation record in the database.
+
+        :param position_id: ID of the position to be liquidated.
+        :return: The created Liquidation record.
+        """
         liquidation_entry = Liquidation(position_id=position_id)
-        db.add(liquidation_entry)
-        db.commit()
-        db.refresh(liquidation_entry)
-        return liquidation_entry
+        return await self.write_to_db(liquidation_entry)
