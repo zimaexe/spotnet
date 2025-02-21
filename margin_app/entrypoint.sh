985a1d3cabe0f3b386651fff81edce848b820c0d
CBoYXD <python.rust.cpp@gmail.com>
2025-02-21 14:33:33 +0200

    Add running alembic

--- margin_app/entrypoint.sh
+++ margin_app/entrypoint.sh
@@ -1,2 +1,5 @@
-uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
+echo "Run migration with alembic"
+poetry run alembic upgrade head
 
+echo "Starting the server ..."
+uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
