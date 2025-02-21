28389c78d0a685b4cb094db44959af9c0ebb1c3d
kamaldeen <kamalaliyu503@gmail.com>
2025-02-21 15:58:25 +0100

    Crud-Completed

---
+++ margin_app/app/crud/liquidation.py
@@ -0,0 +1,17 @@
+"""This module contains the LiquidationCRUD class for managing liquidations."""
+
+from app.crud.base import DBConnector
+from app.models.liquidation import Liquidation
+from sqlalchemy.orm import Session
+
+# pylint: disable=too-few-public-methods
+class LiquidationCRUD(DBConnector):
+    """Handles database operations for liquidations."""
+
+    def liquidate_position(self, db: Session, position_id: int):
+        """Liquidates a position based on the given position ID."""
+        liquidation_entry = Liquidation(position_id=position_id)
+        db.add(liquidation_entry)
+        db.commit()
+        db.refresh(liquidation_entry)
+        return liquidation_entry
