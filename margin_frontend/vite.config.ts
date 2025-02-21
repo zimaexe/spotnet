813514f50e10ebe570923a6bdb0edfd9226f41eb
vadim-ghostman <vadimtovtin@gmail.com>
2025-02-21 22:58:32 +0100

    vite config changes

--- margin_frontend/vite.config.ts
+++ margin_frontend/vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    tailwindcss(),
-    react()
+    react(),
+    tailwindcss()
   ],
 })
