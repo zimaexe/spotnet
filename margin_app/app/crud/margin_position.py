248e5f5d89cd09ab47f423b15d876965be7bcf06
adaniel105 <dakinola18@gmail.com>
2025-02-22 00:47:38 +0100

    fix position create

--- margin_app/app/crud/margin_position.py
+++ margin_app/app/crud/margin_position.py
@@ -11,28 +11,23 @@ from app.models.margin_position import MarginPositionStatus
 
 class MarginPositionCRUD(DBConnector):
     """"Handles margin position database operations"""
-    async def open_margin_position(self, user_id: uuid.UUID, multiplier: int,
-                                   borrowed_amount:int, transaction_id:str) -> MarginPosition:
+    async def open_margin_position(self, user_id: uuid.UUID,
+                                   borrowed_amount:int) -> MarginPosition:
         """
         Opens a margin position by creating an entry record in the database.
         :param user_id: uuid
-        :param multiplier: int
         :param borrowed_amount: int
-        :param transaction_id: str
         :return: MarginPosition
         """
         position_entry = MarginPosition(user_id=user_id,
-                                        multiplier=multiplier,
-                                        borrowed_amount=borrowed_amount,
-                                        status=MarginPositionStatus.OPEN.value,
-                                        transaction_id=transaction_id)
+                                        borrowed_amount=borrowed_amount)
         position = await self.write_to_db(position_entry)
         return position
 
-    async def close_margin_position(self, position_id : str) -> MarginPosition | None:
+    async def close_margin_position(self, position_id : uuid.UUID) -> MarginPosition | None:
         """
         Closes a margin position by updating the position status in the database.
-        :param position_id: str
+        :param position_id: uuid
         :return: Position | None
         """
         position = self.get_object(MarginPosition, position_id)
