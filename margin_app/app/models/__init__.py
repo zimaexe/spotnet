e7896232112e049380df654ed524ecb492688df2
djeck1432 <djeck1432@users.noreply.github.com>
2025-02-21 10:20:02 +0100

    Update margin_app/app/models/__init__.py

--- margin_app/app/models/__init__.py
+++ margin_app/app/models/__init__.py
@@ -5,5 +5,5 @@ Initialization for the models package.
 from app.models.base import BaseModel
 from app.models.user import User
 from app.models.liquidation import Liquidation
-from .deposit import Deposit
+from app.deposit import Deposit
 
