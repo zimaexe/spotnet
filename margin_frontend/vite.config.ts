4b0adca28af093aa8a51b817d696edd5918da3db
giafratte <kikelomokhadijatibinola@yahoo.com>
2025-02-23 01:10:56 +0100

    chore: move test functionality to margin_frontend folder

--- margin_frontend/vite.config.ts
+++ margin_frontend/vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-})
+  plugins: [react(), tailwindcss()],
+  test: {
+    environment: "jsdom",
+  },
+});
