33676d6de9de2068ccc6e1d3ab6c313729781a59
whateverfw <olegandriiv0@gmail.com>
2025-02-25 01:11:57 +0200

    fix: missing tailwindcss plugin

--- margin_frontend/vite.config.ts
+++ margin_frontend/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   test: {
     name: "react",
     browser: {
