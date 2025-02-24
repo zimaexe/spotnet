fabb7aa3774db8b0cce0ee16744cdf65a1b89167
Binayak <76868488+binayak9932@users.noreply.github.com>
2025-02-24 15:28:06 +0000

    fixes

---
+++ margin_app/app/tests/conftest.py
@@ -0,0 +1,23 @@
+
+
+import uuid
+import pytest_asyncio
+from app.crud.base import DBConnector
+from app.models.base import BaseModel
+
+from sqlalchemy import Column, String
+
+
+
+@pytest_asyncio.fixture
+async def db_connector():
+    """Fixture to create a database connection for testing."""
+    db = DBConnector()
+    async with db.engine.begin() as conn:
+        await conn.run_sync(BaseModel.metadata.create_all)
+
+    try:
+        yield db
+    finally:
+        async with db.engine.begin() as conn:
+            await conn.run_sync(BaseModel.metadata.drop_all)
--- margin_app/app/tests/test_db_connector.py
+++ margin_app/app/tests/test_db_connector.py
@@ -28,18 +28,6 @@ class TestModel(BaseModel):
     name = Column(String, nullable=False)
 
 
-@pytest_asyncio.fixture
-async def db_connector():
-    """Fixture to create a database connection for testing."""
-    db = DBConnector()
-    async with db.engine.begin() as conn:
-        await conn.run_sync(BaseModel.metadata.create_all)
-
-    try:
-        yield db
-    finally:
-        async with db.engine.begin() as conn:
-            await conn.run_sync(BaseModel.metadata.drop_all)
 
 
 @pytest_asyncio.fixture
