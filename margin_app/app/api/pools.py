71f282f27aa4a9480d819cbfc23401ec81a3da05
waterscape03 <dm.kobek@gmail.com>
2025-02-25 19:06:52 +0100

    Add endpoint for create pool

--- margin_app/app/api/pools.py
+++ margin_app/app/api/pools.py
@@ -2,13 +2,53 @@
 This module contains the API routes for the pools.
 """
 
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, Depends
 from loguru import logger
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.crud.pool import pool_crud
 from app.crud.pool import user_pool_crud
-from app.schemas.pools import UserPoolResponse, UserPoolCreate
+from app.schemas.pools import (
+    UserPoolResponse,
+    UserPoolCreate,
+    PoolCreate,
+    PoolResponse,
+    PoolRiskStatus,
+)
+from app.db.sessions import get_db
 
 router = APIRouter()
+
+
+@router.post(
+    "/create_pool", response_model=PoolResponse, status_code=status.HTTP_201_CREATED
+)
+async def create_pool(
+    token: str, risk_status: PoolRiskStatus, db: AsyncSession = Depends(get_db)
+) -> PoolResponse:
+    """
+    Create a new pool
+
+    :param token: pool token (path parameter)
+    :param risk_status: pool risk status
+    :param db: database session
+    :return: created pool
+    """
+    try:
+        created_pool = await pool_crud.create_pool(token=token, risk_status=risk_status)
+        if not created_pool:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Pool was not created.",
+            )
+    except Exception as e:
+        logger.error(f"Error creating pool: {e}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Something went wrong.",
+        ) from e
+
+    return created_pool
 
 
 @router.post(
--- margin_app/app/schemas/pools.py
+++ margin_app/app/schemas/pools.py
@@ -4,7 +4,9 @@ This module contains Pydantic schemas for Pools.
 
 from pydantic import BaseModel, ConfigDict
 from uuid import UUID
+from typing import Optional
 from decimal import Decimal
+from app.models.pool import PoolRiskStatus
 
 
 class UserPoolBase(BaseModel):
@@ -28,6 +30,37 @@ class UserPoolCreate(UserPoolBase):
 class UserPoolResponse(UserPoolBase):
     """
     User pool response model
+    """
+
+    id: UUID
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class PoolBase(BaseModel):
+    """
+    Pool base model
+    """
+
+    name: Optional[str] = None
+    risk_status: Optional[PoolRiskStatus] = PoolRiskStatus.LOW
+
+
+class PoolCreate(PoolBase):
+    """
+    Pool create model
+    """
+
+
+class PoolUpdate(PoolBase):
+    """
+    Pool update model
+    """
+
+
+class PoolResponse(PoolBase):
+    """
+    Pool response model
     """
 
     id: UUID
