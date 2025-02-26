d1f72bb8ba55c06de06bb5701a57c35636873940
enyinnaya1234 <chemicalnilton@gmail.com>
2025-02-26 12:18:41 +0100

    refac: change the config files

--- margin_frontend/vite.config.ts
+++ margin_frontend/vite.config.ts
@@ -1,27 +1,24 @@
-import { defineConfig } from "vitest/config";
-import react from "@vitejs/plugin-react";
+import { resolve } from "node:path";
 import tailwindcss from "@tailwindcss/vite";
-import { resolve } from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "src"),
-      "@routes": resolve(__dirname, "src/routes"),
-      "@utils": resolve(__dirname, "src/utils"),
-      "@ui": resolve(__dirname, "src/ui"),
-      "@assets": resolve(__dirname, "src/assets"),
-    },
-  },
+	plugins: [react(), tailwindcss()],
+	resolve: {
+		alias: {
+			"@": resolve(__dirname, "src"),
+			
+		},
+	},
 
-  test: {
-    name: "react",
-    browser: {
-      enabled: true,
-      provider: "playwright",
-      headless: true,
-    },
-    exclude: ["test/browser/**/*"],
-  },
+	test: {
+		name: "react",
+		browser: {
+			enabled: true,
+			provider: "playwright",
+			headless: true,
+		},
+		exclude: ["test/browser/**/*"],
+	},
 });
