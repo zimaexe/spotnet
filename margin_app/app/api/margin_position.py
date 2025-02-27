5889e5e663f81ef2d0ff166497bbedc687c3a85b
djeck1432 <djeck1432@users.noreply.github.com>
2025-02-27 18:43:00 +0100

    Update margin_position.py

--- margin_app/app/api/margin_position.py
+++ margin_app/app/api/margin_position.py
@@ -9,7 +9,6 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.crud.margin_position import margin_position_crud
-from app.db.sessions import get_db
 from app.schemas.margin_position import (
     CloseMarginPositionResponse,
     MarginPositionCreate,
@@ -20,10 +19,7 @@ router = APIRouter()
 
 
 @router.post("/open", response_model=MarginPositionResponse)
-async def open_margin_position(
-    position_data: MarginPositionCreate,
-    db: AsyncSession = Depends(get_db),
-):
+async def open_margin_position(position_data: MarginPositionCreate):
     """
     Opens a margin position by creating an entry record in the database.
     :param position_data: MarginPositionCreate
@@ -41,10 +37,7 @@ async def open_margin_position(
 
 
 @router.post("/close/{position_id}", response_model=CloseMarginPositionResponse)
-async def close_margin_position(
-    position_id: UUID, 
-    db: AsyncSession = Depends(get_db)
-) -> CloseMarginPositionResponse:
+async def close_margin_position(position_id: UUID) -> CloseMarginPositionResponse:
     """
     Close a margin position endpoint.
 
@@ -58,7 +51,6 @@ async def close_margin_position(
     Raises:
         HTTPException: 404 error if the position is not found
     """
-    margin_position_crud.db = db
     status = await margin_position_crud.close_margin_position(position_id)
 
     if not status:
