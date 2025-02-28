ae6cbe3c865d58a554561349050e41c175832e67
whateverfw <olegandriiv0@gmail.com>
2025-02-28 18:05:03 +0200

    fix: test

--- margin_frontend/test/HelloWorld.spec.test.jsx
+++ margin_frontend/test/HelloWorld.spec.test.jsx
@@ -1,15 +1,11 @@
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import { describe, it } from "vitest";
-import HelloWorld from "../components/HelloWorld";
+import { Title } from "../src/ui/layout/title";
 
-describe("HelloWorld test component", () => {
-	it("renders name and counter works", async () => {
-		render(<HelloWorld name="Vitest" />);
+describe("Vitest test component", () => {
+  it("renders title", async () => {
+    render(<Title title="Vitest" />);
 
-		await screen.findByText("Hello Vitest x1!");
-		await userEvent.click(screen.getByRole("button", { name: "Increment" }));
-
-		await screen.findByText("Hello Vitest x2!");
-	});
+    await screen.findByText("Vitest");
+  });
 });
