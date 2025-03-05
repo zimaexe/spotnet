24271d2afa659e06dd5117ce6493552fb915b3d6
UzNaZ <uzik.bs@gmail.com>
2025-03-05 22:44:08 +0100

    adjusting for pylint

--- margin_app/app/models/order.py
+++ margin_app/app/models/order.py
@@ -1,3 +1,7 @@
+"""
+Order model
+"""
+
 from sqlalchemy import VARCHAR, Numeric, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column
@@ -12,7 +16,8 @@ class Order(BaseModel):
     Attributes:
     - user_id (UUID): The unique identifier of the user who placed the order.
                       Acts as both a primary key and a foreign key referencing the "user" table.
-    - price (float): The price of the order, stored with a precision of up to 10 digits and 2 decimal places.
+    - price (float): The price of the order,
+    stored with a precision of up to 10 digits and 2 decimal places.
     - token (str): The token associated with the order.
     - position (UUID): The identifier of the position related to the order.
 
@@ -20,7 +25,8 @@ class Order(BaseModel):
     - Name: "order"
     """
     __tablename__ = "order"
-    user_id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), ForeignKey("user.id"), primary_key=True)
+    user_id: Mapped[UUID] = mapped_column(UUID(as_uuid=True),
+                                          ForeignKey("user.id"), primary_key=True)
     price: Mapped[float] = mapped_column(Numeric(precision=10, scale=2))
     token: Mapped[str] = mapped_column(VARCHAR)
     position: Mapped[UUID] = mapped_column(UUID(as_uuid=True))
