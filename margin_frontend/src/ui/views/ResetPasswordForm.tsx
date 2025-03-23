41b9005a31b7c046f39746338e3f63d4e2551454
Valera56 <203841869+Valera56pub@users.noreply.github.com>
2025-03-23 18:28:45 +0200

    Update margin_frontend/src/ui/views/ResetPasswordForm.tsx
    Co-authored-by: Vadym <78219653+vadim-ghostman@users.noreply.github.com>

--- margin_frontend/src/ui/views/ResetPasswordForm.tsx
+++ margin_frontend/src/ui/views/ResetPasswordForm.tsx
@@ -46,7 +46,7 @@ const ResetPasswordForm = () => {
                         onChange={(e) => onChange(e.target.value)} />
                     <svg
                         onClick={() => setPasswordVisibility(!passwordVisible)}
-                        className="shrink-0 size-3.5 absolute right-4 cursor-pointer" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        className="shrink-0 size-3.5 absolute right-4 cursor-pointer" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                         <path className="hs-password-active:hidden" d="M9.88 9.88a3 3 0 1 0 4.24 4.24"></path>
                         <path className="hs-password-active:hidden" d="M10.73 5.08A10.43 10.43 0 0 1 12 5c7 0 10 7 10 7a13.16 13.16 0 0 1-1.67 2.68"></path>
                         <path className="hs-password-active:hidden" d="M6.61 6.61A13.526 13.526 0 0 0 2 12s3 7 10 7a9.74 9.74 0 0 0 5.39-1.61"></path>
