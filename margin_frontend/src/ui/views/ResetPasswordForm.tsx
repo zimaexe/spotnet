b1db1b37f0fc118213b0e4dac6775d49662196fc
Valera56 <203841869+Valera56pub@users.noreply.github.com>
2025-03-23 17:01:31 +0200

    improve UX

--- margin_frontend/src/ui/views/ResetPasswordForm.tsx
+++ margin_frontend/src/ui/views/ResetPasswordForm.tsx
@@ -5,15 +5,25 @@ import { Button } from '../core/button'
 
 
 const ResetPasswordForm = () => {
-    const [passwordVisible, setPasswordVisibility] = useState(false);
+
+    const [resetDone, setResetDone] = useState(false);
+    const [passwordVisibleNew, setPasswordNewVisibility] = useState(false);
+    const [passwordVisibleConfirm, setPasswordConfirmVisibility] = useState(false);
     const [newInput, setNewInput] = useState('');
     const [confirmInput, setConfirmInput] = useState('');
 
     const onSubmit = async () => {
         console.log('onSubmit');
+        setResetDone(true);
     }
 
-    const passwordInput = (label: string, onChange: (value: string) => void) => {
+    const passwordInput = (label: string,
+        onChange: (value: string) => void,
+        passwordVisible: boolean,
+        setPasswordVisibility: React.Dispatch<React.SetStateAction<boolean>>
+    ) => {
+
+
         return (
             <div className='flex flex-col'>
                 <label>{label}</label>
@@ -21,7 +31,7 @@ const ResetPasswordForm = () => {
 
                     <Input
                         className='w-100 pr-10'
-                        type={passwordVisible ?'text': 'password'}
+                        type={passwordVisible ? 'text' : 'password'}
                         onChange={(e) => onChange(e.target.value)} />
                     <svg
                         onClick={() => setPasswordVisibility(!passwordVisible)}
@@ -29,9 +39,9 @@ const ResetPasswordForm = () => {
                         <path className="hs-password-active:hidden" d="M9.88 9.88a3 3 0 1 0 4.24 4.24"></path>
                         <path className="hs-password-active:hidden" d="M10.73 5.08A10.43 10.43 0 0 1 12 5c7 0 10 7 10 7a13.16 13.16 0 0 1-1.67 2.68"></path>
                         <path className="hs-password-active:hidden" d="M6.61 6.61A13.526 13.526 0 0 0 2 12s3 7 10 7a9.74 9.74 0 0 0 5.39-1.61"></path>
-                        <line className={passwordVisible? "hidden":''} x1="2" x2="22" y1="2" y2="22"></line>
-                        <path className={!passwordVisible? "hidden":''} d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z"></path>
-                        <circle className={!passwordVisible? "hidden":''} cx="12" cy="12" r="3"></circle>
+                        <line className={passwordVisible ? "hidden" : ''} x1="2" x2="22" y1="2" y2="22"></line>
+                        <path className={!passwordVisible ? "hidden" : ''} d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z"></path>
+                        <circle className={!passwordVisible ? "hidden" : ''} cx="12" cy="12" r="3"></circle>
                     </svg>
                 </div>
             </div>
@@ -39,26 +49,64 @@ const ResetPasswordForm = () => {
     }
 
     return (
-        <Card className='text-white flex gap-4 flex-col'>
-            <h1 className='font-bold text-lg'>Set new Password</h1>
-            <form action="" className='flex gap-2 flex-col' onSubmit={(e) => {e.preventDefault();onSubmit()}}>
-                {passwordInput("Password", setNewInput)}
-                {passwordInput("Confirm", setConfirmInput)}
+        <Card className='text-white flex gap-6 flex-col'>
+            {!resetDone ? (
+                <>
+                    <header>
+                        <h1 className='font-bold text-xl'>Set new Password</h1>
+                        <div className='text-gray-400 text-sm'>
+                            Fill in your new password and confirm it
+                        </div>
+                    </header>
 
-                <div className='text-red-400 h-4'>
-                    {(newInput !== confirmInput && confirmInput.length > 0) && "Your passwords do not match"}
-                </div>
+                    <form action="" className='flex gap-4 flex-col' onSubmit={(e) => { e.preventDefault(); onSubmit() }}>
+                        {passwordInput("Password", setNewInput,passwordVisibleNew,setPasswordNewVisibility)}
+                        {passwordInput("Confirm", setConfirmInput,passwordVisibleConfirm,setPasswordConfirmVisibility)}
 
-                <Button
-                    variant={"outline"}
-                    size={"md"}
-                    type='submit'
-                    disabled={(newInput !== confirmInput) || !newInput.length}
-                    className='mt-4'
-                >
-                    Submit
-                </Button>
-            </form>
+                        <div className='text-red-400 h-4'>
+                            {(newInput !== confirmInput && confirmInput.length > 0) && "Your passwords do not match"}
+                        </div>
+
+                        <Button
+                            variant={"outline"}
+                            size={"md"}
+                            type='submit'
+                            disabled={(newInput !== confirmInput) || !newInput.length}
+                            className='mt-1'
+                        >
+                            Submit
+                        </Button>
+                    </form>
+                </>
+            ) : (
+                <>
+                    <header>
+                        <h1 className='font-bold text-xl'>Reset done</h1>
+                        <div className='text-gray-400 text-sm'>
+                            Your password has been successfully reset
+                        </div>
+                    </header>
+
+                    <Button
+                        variant={"outline"}
+                        size={"md"}
+                        onClick={() => setResetDone(false)}
+                        className='mt-1'
+                    >
+                        Continue
+                    </Button>
+                </>
+            )}
+
+
+            <div className='text-center flex gap-1 justify-center'>
+                <span>
+                    Back to
+                </span>
+                <a className="text-indigo-600 hover:underline" href="/sign-in">
+                    Sign in
+                </a>
+            </div>
         </Card>
     )
 }
