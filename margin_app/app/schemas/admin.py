a8368646c72f51080edb03a2855793b859400add
zimaexe <zima2908@gmail.com>
2025-03-26 19:30:37 +0100

    added new scheme for admin reset password

--- margin_app/app/schemas/admin.py
+++ margin_app/app/schemas/admin.py
@@ -3,6 +3,7 @@ This module contains Pydantic schemas for admin.
 """
 from uuid import UUID
 
+from typing import Optional
 from pydantic import BaseModel, EmailStr
 
 
@@ -21,6 +22,15 @@ class AdminRequest(AdminBase):
     """
 
     password: str
+
+
+class AdminResetPassword(AdminBase):
+    """
+    Admin reset password model
+    """
+    name: Optional[str] = None
+    old_password: str
+    new_password: str
 
 
 class AdminResponse(AdminBase):
