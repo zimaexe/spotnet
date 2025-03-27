3d2bb01a7b2db344a208b3dc4d57a287fc5985a5
Donyk-s <doncarlos@i.ua>
2025-03-27 15:38:46 +0200

    Use Dashboard component in src/routes/dashboard.tsx

--- margin_frontend/src/routes/dashboard.tsx
+++ margin_frontend/src/routes/dashboard.tsx
@@ -1,14 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
+import Dashboard from '../ui/dashboard/Dashboard'; 
 
 export const Route = createFileRoute("/dashboard")({
     component: Dashboard,
 });
-
-function Dashboard() {
-    return (
-        <div className="p-4">
-            <h1 className="text-3xl font-bold text-baseWhite">Dashboard Works!</h1>
-            <p className="text-baseWhite">This is a test dashboard.</p>
-        </div>
-    );
-}
