ceec4edbe6e93e506dab430a8b8b46ef7de4e3c5
Donyk-s <doncarlos@i.ua>
2025-03-27 13:25:01 +0200

    Change store export to default in src/store/store.ts

--- margin_frontend/src/store/store.ts
+++ margin_frontend/src/store/store.ts
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cryptoDashboardReducer from './cryptoDashboard/cryptoDashboardSlice';
-
-export const store = configureStore({
+ const store = configureStore({
   reducer: {
     cryptoDashboard: cryptoDashboardReducer,
   },
 });
+export default store;
