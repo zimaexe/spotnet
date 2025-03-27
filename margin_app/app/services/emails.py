598ce7eef17e994709fe373dbf513b46db92e0d6
zimaexe <zima2908@gmail.com>
2025-03-27 18:58:36 +0100

    pylint fixed

--- margin_app/app/services/emails.py
+++ margin_app/app/services/emails.py
@@ -73,8 +73,11 @@ class EmailService:
         Returns:
             Coroutine: A coroutine that sends the email asynchronously.
         """
-        token = create_access_token(email=to_email,
-                                    expires_delta=get_expire_time(settings.reset_password_expire_minutes))
+        token = create_access_token(
+            email=to_email,
+            expires_delta=get_expire_time(
+                settings.reset_password_expire_minutes),
+        )
         return await self.send_email(
             to_email=to_email,
             subject="Reset your password",
