31af16c75905909f16d9c33bddac7b518ad56f1e
vadim-ghostman <vadimtovtin@gmail.com>
2025-03-30 22:43:22 +0200

    added state after email entering & fix conflicts

--- margin_frontend/src/routeTree.gen.ts
+++ margin_frontend/src/routeTree.gen.ts
@@ -10,40 +10,32 @@
 
 // Import Routes
 
-<<<<<<< HEAD
-import { Route as rootRoute } from "./routes/__root";
-import { Route as TradeImport } from "./routes/trade";
-import { Route as ResetpasswordImport } from "./routes/reset_password";
-import { Route as PoolImport } from "./routes/pool";
-import { Route as IndexImport } from "./routes/index";
-=======
 import { Route as rootRoute } from './routes/__root'
 import { Route as TradeImport } from './routes/trade'
+import { Route as ResetpasswordImport } from './routes/reset_password'
 import { Route as PoolImport } from './routes/pool'
 import { Route as ChangePasswordImport } from './routes/change-password'
 import { Route as IndexImport } from './routes/index'
->>>>>>> upstream/main
 
 // Create/Update Routes
 
 const TradeRoute = TradeImport.update({
-  id: "/trade",
-  path: "/trade",
+  id: '/trade',
+  path: '/trade',
   getParentRoute: () => rootRoute,
-} as any);
+} as any)
 
 const ResetpasswordRoute = ResetpasswordImport.update({
-  id: "/reset_password",
-  path: "/reset_password",
+  id: '/reset_password',
+  path: '/reset_password',
   getParentRoute: () => rootRoute,
-} as any);
-
+} as any)
 
 const PoolRoute = PoolImport.update({
-  id: "/pool",
-  path: "/pool",
+  id: '/pool',
+  path: '/pool',
   getParentRoute: () => rootRoute,
-} as any);
+} as any)
 
 const ChangePasswordRoute = ChangePasswordImport.update({
   id: '/change-password',
@@ -52,45 +44,15 @@ const ChangePasswordRoute = ChangePasswordImport.update({
 } as any)
 
 const IndexRoute = IndexImport.update({
-  id: "/",
-  path: "/",
+  id: '/',
+  path: '/',
   getParentRoute: () => rootRoute,
-} as any);
+} as any)
 
 // Populate the FileRoutesByPath interface
 
-declare module "@tanstack/react-router" {
+declare module '@tanstack/react-router' {
   interface FileRoutesByPath {
-<<<<<<< HEAD
-    "/": {
-      id: "/";
-      path: "/";
-      fullPath: "/";
-      preLoaderRoute: typeof IndexImport;
-      parentRoute: typeof rootRoute;
-    };
-    "/pool": {
-      id: "/pool";
-      path: "/pool";
-      fullPath: "/pool";
-      preLoaderRoute: typeof PoolImport;
-      parentRoute: typeof rootRoute;
-    };
-    "/reset_password": {
-      id: "/reset_password";
-      path: "/reset_password";
-      fullPath: "/reset_password";
-      preLoaderRoute: typeof ResetpasswordImport;
-      parentRoute: typeof rootRoute;
-    };
-    "/trade": {
-      id: "/trade";
-      path: "/trade";
-      fullPath: "/trade";
-      preLoaderRoute: typeof TradeImport;
-      parentRoute: typeof rootRoute;
-    };
-=======
     '/': {
       id: '/'
       path: '/'
@@ -112,6 +74,13 @@ declare module "@tanstack/react-router" {
       preLoaderRoute: typeof PoolImport
       parentRoute: typeof rootRoute
     }
+    '/reset_password': {
+      id: '/reset_password'
+      path: '/reset_password'
+      fullPath: '/reset_password'
+      preLoaderRoute: typeof ResetpasswordImport
+      parentRoute: typeof rootRoute
+    }
     '/trade': {
       id: '/trade'
       path: '/trade'
@@ -119,53 +88,16 @@ declare module "@tanstack/react-router" {
       preLoaderRoute: typeof TradeImport
       parentRoute: typeof rootRoute
     }
->>>>>>> upstream/main
   }
 }
 
 // Create and export the route tree
 
 export interface FileRoutesByFullPath {
-<<<<<<< HEAD
-  "/": typeof IndexRoute;
-  "/pool": typeof PoolRoute;
-  "/reset_password": typeof ResetpasswordRoute;
-  "/trade": typeof TradeRoute;
-}
-
-export interface FileRoutesByTo {
-  "/": typeof IndexRoute;
-  "/pool": typeof PoolRoute;
-  "/reset_password": typeof ResetpasswordRoute;
-  "/trade": typeof TradeRoute;
-}
-
-export interface FileRoutesById {
-  __root__: typeof rootRoute;
-  "/": typeof IndexRoute;
-  "/pool": typeof PoolRoute;
-  "/reset_password": typeof ResetpasswordRoute;
-  "/trade": typeof TradeRoute;
-}
-
-export interface FileRouteTypes {
-  fileRoutesByFullPath: FileRoutesByFullPath;
-  fullPaths: "/" | "/pool" | "/reset_password" | "/trade";
-  fileRoutesByTo: FileRoutesByTo;
-  to: "/" | "/pool" | "/reset_password" | "/trade";
-  id: "__root__" | "/" | "/pool" | "/reset_password" | "/trade";
-  fileRoutesById: FileRoutesById;
-}
-
-export interface RootRouteChildren {
-  IndexRoute: typeof IndexRoute;
-  PoolRoute: typeof PoolRoute;
-  ResetpasswordRoute: typeof ResetpasswordRoute;
-  TradeRoute: typeof TradeRoute;
-=======
   '/': typeof IndexRoute
   '/change-password': typeof ChangePasswordRoute
   '/pool': typeof PoolRoute
+  '/reset_password': typeof ResetpasswordRoute
   '/trade': typeof TradeRoute
 }
 
@@ -173,6 +105,7 @@ export interface FileRoutesByTo {
   '/': typeof IndexRoute
   '/change-password': typeof ChangePasswordRoute
   '/pool': typeof PoolRoute
+  '/reset_password': typeof ResetpasswordRoute
   '/trade': typeof TradeRoute
 }
 
@@ -181,15 +114,22 @@ export interface FileRoutesById {
   '/': typeof IndexRoute
   '/change-password': typeof ChangePasswordRoute
   '/pool': typeof PoolRoute
+  '/reset_password': typeof ResetpasswordRoute
   '/trade': typeof TradeRoute
 }
 
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
-  fullPaths: '/' | '/change-password' | '/pool' | '/trade'
+  fullPaths: '/' | '/change-password' | '/pool' | '/reset_password' | '/trade'
   fileRoutesByTo: FileRoutesByTo
-  to: '/' | '/change-password' | '/pool' | '/trade'
-  id: '__root__' | '/' | '/change-password' | '/pool' | '/trade'
+  to: '/' | '/change-password' | '/pool' | '/reset_password' | '/trade'
+  id:
+    | '__root__'
+    | '/'
+    | '/change-password'
+    | '/pool'
+    | '/reset_password'
+    | '/trade'
   fileRoutesById: FileRoutesById
 }
 
@@ -197,8 +137,8 @@ export interface RootRouteChildren {
   IndexRoute: typeof IndexRoute
   ChangePasswordRoute: typeof ChangePasswordRoute
   PoolRoute: typeof PoolRoute
+  ResetpasswordRoute: typeof ResetpasswordRoute
   TradeRoute: typeof TradeRoute
->>>>>>> upstream/main
 }
 
 const rootRouteChildren: RootRouteChildren = {
@@ -207,11 +147,11 @@ const rootRouteChildren: RootRouteChildren = {
   PoolRoute: PoolRoute,
   ResetpasswordRoute: ResetpasswordRoute,
   TradeRoute: TradeRoute,
-};
+}
 
 export const routeTree = rootRoute
   ._addFileChildren(rootRouteChildren)
-  ._addFileTypes<FileRouteTypes>();
+  ._addFileTypes<FileRouteTypes>()
 
 /* ROUTE_MANIFEST_START
 {
